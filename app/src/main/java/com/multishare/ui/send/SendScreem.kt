ede7ff3c0a5266430f0c5259c474f591ad9ed8f6
priyanshu <104priyanshu@gmail.com>
2024-10-17 14:52:19 +0530

    New D temp commit

---
+++ app/src/main/java/com/multishare/ui/send/SendScreem.kt
@@ -0,0 +1,12 @@
+package com.multishare.ui.send
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun sendScreen(){
+
+
+
+}
