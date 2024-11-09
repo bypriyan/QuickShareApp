1a491290ccdb0640ab4ee14da718a16e38c4fdb6
priyanshu <104priyanshu@gmail.com>
2024-11-09 11:45:47 +0530

    init new

--- TVMultishare/app/src/main/java/com/multishare/ui/theme/home/loginScreen.kt
+++
@@ -1,2 +0,0 @@
-package com.multishare.ui.theme.home
-
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,22 @@
+import org.gradle.kotlin.dsl.android
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
 
 android {
-    namespace = "com.tvmultishare"
-    compileSdk = 34
+    namespace = "com.bypriyan.multishare"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.tvmultishare"
+        applicationId = "com.bypriyan.multishare"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
+        multiDexEnabled = true
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -29,6 +31,10 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,32 +42,50 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    //dagger hilt
+
+    //hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    //multidex
+    implementation ("androidx.annotation:annotation:1.6.0")
+    implementation ("org.jetbrains:annotations:24.0.1")
+    implementation ("androidx.multidex:multidex:2.0.1")
+    //sdp ssp
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
+    implementation ("com.intuit.ssp:ssp-android:1.1.0")
+    //lottie anim
+    implementation ("com.airbnb.android:lottie:6.4.0")
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    //mvvm
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    // Lifecycles only (without ViewModel or LiveData)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    // LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    //anim
+    implementation ("com.flaviofaria:kenburnsview:1.0.7")
+    //coil
+    implementation("io.coil-kt:coil:2.6.0")
 
+    //splash screen api
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
 }
 
